d51e8c80c41f704cff9fd872813cabfa26dd3d84
Your Name <your@email.com>
2026-05-30 20:54:17 +0330

    Keep only .cs and image files (aligned with QuantumRefrigiz structure)

--- SugaR-master/source/Build x64.cmd
+++
@@ -1,9 +0,0 @@
-@echo off
-
-call make clean
-call mingw32-make profile-build ARCH=x86-64 COMP=mingw CXX=x86_64-w64-mingw32-g++ -j14
-call strip sugar.exe
-call ren sugar.exe sugar1.5-x86-64.exe
-
-call make clean
-call pause
